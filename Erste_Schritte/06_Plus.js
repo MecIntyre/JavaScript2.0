fa3f12cad55a271490d7ad3fb1cd570e50f1e874
MecIntyre <abuesswalker@gmail.com>
2022-12-05 11:39:55 +0100

    Addition & Verknüpfung (String / Integer)

---
+++ Erste_Schritte/06_Plus.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// Numerische Addition
+console.log(5 + 6)
+
+// Verknüpfen von Strings
+console.log("5" + "6")
+
+// Verknüpfen von Strings
+console.log("Hallo, " + "JavaScript!")
+
+// Addition von Boleans
+console.log(true + true)
+
+// Beispiel Addition
+let water = 1.9
+
+let amount1 = 10 + water
+console.log(amount1)
+
+//Fehlerbeispiel Addition & Verknüpfung bei Integer + String
+let amount2 = 10 + "1.9"
+console.log(amount2)
