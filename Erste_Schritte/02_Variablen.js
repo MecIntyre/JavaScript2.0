38885ea8f365afe7ba6a67f8ecbbd378f01c05a3
MecIntyre <abuesswalker@gmail.com>
2022-12-05 11:00:39 +0100

    Variable ertsellen & zuweisen

---
+++ Erste_Schritte/02_Variablen.js
@@ -0,0 +1,12 @@
+// Variable "greeting" wird erstellt
+let greeting
+
+// Der Variable "greeting" wird ein Wert zugewiesen
+greeting = "Hallo Welt!"
+
+// Die Variable2" wird erstellt und ihr wird ein Wert zugewiesen
+let greeting2 = "Hallo Welt!"
+
+// Die Variable "greeting2" wird 2x ausgegeben
+console.log(greeting2)
+console.log(greeting2)
