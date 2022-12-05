8af17c764861fd1f853499e2c11c20917dead931
MecIntyre <abuesswalker@gmail.com>
2022-12-05 11:20:24 +0100

    Definition & erkennen

---
+++ Erste_Schritte/05_Datentypen.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// String
+let a = "Hallo Welt!"       // "" & '' möglich
+console.log(typeof a)
+console.log(a)
+
+// Integer -> Zahlen
+let b = 42.5
+console.log(typeof b)
+console.log(b)
+
+// Boleans (true / false)
+let c = true
+console.log(typeof c)
+console.log(c)
