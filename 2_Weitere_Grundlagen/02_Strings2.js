bf7a1898959d45e57993aadf2b57f567b797da2d
MecIntyre <abuesswalker@gmail.com>
2022-12-06 13:49:12 +0100

    Weitere Grundlagen von Zeichenketten

---
+++ 2_Weitere_Grundlagen/02_Strings2.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Newline
+console.log("Hallo\nWelt!")
+
+// String wiederholen
+console.log("-".repeat(20))
+
+// .slice(start, end)
+console.log("Hallo Welt!".slice(6, 8))
+
+// .subtr(start, length)
+console.log("Hallo Welt!".substr(6, 2))
