4d03e70def37105d175b41260e8ff2540b06826f
MecIntyre <abuesswalker@gmail.com>
2022-12-07 11:26:08 +0100

    Aufgabe finalisiert

--- 2_Weitere_Grundlagen/03_Aufgabe_Strings.js
+++ 2_Weitere_Grundlagen/03_Aufgabe_Strings.js
@@ -9,19 +9,33 @@ sowie ein paar Plus-Zeichen eingelesen, die im Original gar nicht existieren.
 Das Möchten wir korrigieren!
 
 Aufgabe 1:  Entferne die Leerzeichen links und rechts und schreibe das Ergebnis zurück
-            in die Variable Shopping
-            
-Aufgabe 2:  Finde die Position der drei Plus-Zeichen und speichere diese in einer Variable
+            in die Variable Shopping */
+console.log("Lösung 1")
+shopping = shopping.trim() 
+console.log(shopping)
 
+
+// Aufgabe 2:  Finde die Position der drei Plus-Zeichen und speichere diese in einer Variable
+console.log("\nLösung 2")
+let plus = shopping.indexOf("+++")
+console.log(plus)
+
+/*
 Aufgabe 3:  Schneide die 3 Plus-Zeichen aus und gebe das Ergebnis mit einem console.log aus.
             Verwende dazu einmal die substr-Funktion und einmal die slice-Funktion.
-            
-Es soll ausgegeben werden: "Apfel, Pfirsich, Avocado, Mango"
-Tipp:   Du wirst hierzu 2 Aufrufe der jeweiligen Funktionen benötigen - 
-        einmal bis hin zu den Plus-Zeichen und einmal für das Stück danach!
+            Es soll ausgegeben werden: "Apfel, Pfirsich, Avocado, Mango"
+Tipp:       Du wirst hierzu 2 Aufrufe der jeweiligen Funktionen benötigen - 
+            einmal bis hin zu den Plus-Zeichen und einmal für das Stück danach! */
+console.log("\nLösung 3")
+console.log(shopping.substr(0, plus) + shopping.substr(plus + 3))
+console.log(shopping.slice(0, plus) + shopping.slice(plus + 3))
 
-AUfgabe 4:  Schneide die 3 Plus-Zeichen aus und verwende dazu die replace-Funktion!
+// Aufgabe 4:  Schneide die 3 Plus-Zeichen aus und verwende dazu die replace-Funktion!
+console.log("\nLösung 4")
+shopping = shopping.replace("+++", "")
+console.log(shopping)
 
+/*
 Aufgabe 5:  Jetzt soll die Shopping-Liste ausgegeben werden.
             Wandle die Liste in folgende Schreibweise um.
             Platziere auch die 2 Striche über & unter der Auflistung und verwende
@@ -46,28 +60,8 @@ Tipp:   Du kannst z.B. mit Hilfe der replace-Funktion immer nur ein Komma,
         
         Du darfst wahlweise alles in einem console.log-Aufruf ausgeben oder
         auch mehrere console.log-Aufrufe verwenden. */
-
-// Lösung zu Aufgabe 1:
-shopping = shopping.trim() 
-console.log(shopping)
-
-// Lösung zu Aufgabe 2:
-let plus
-plus = shopping.indexOf("+++")
-console.log(plus)
-
-// Lösung zu Aufgabe 3:
-console.log(shopping.slice(17, 20))
-console.log(shopping.substr(17, 3))
-
-// Lösung zu Aufgabe 4:
-shopping = shopping.replace("+++", "")
-console.log(shopping)
-
-// Lösung zu Aufgabe 5:
+console.log("\nLösung 5")
 console.log("-".repeat(20))
-console.log("- " + shopping.replace(",", "\n").replace("Pfirsich", "").replace("Avocado", "").replace("Mango", ""))
-console.log("- " + shopping.replace(",", "\n").replace("Apfel", "").replace("Avocado", "").replace("Mango", ""))
-console.log("- " + shopping.replace(",", "\n").replace("Apfel", "").replace("Pfirsich", "").replace("Mango", ""))
-console.log("- " + shopping.replace(",", "\n").replace("Apfel", "").replace("Pfirsich", "").replace("Avocado", ""))
+console.log("Einkaufsliste:")
+console.log("- " + shopping.replace(", ", "\n - ").replace(", ", "\n - ").replace(", ", "\n - "))
 console.log("-".repeat(20))
