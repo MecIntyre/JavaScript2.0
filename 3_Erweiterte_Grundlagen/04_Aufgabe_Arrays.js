4d7cafc89fe91b00b158ca7867bb7e79d13d54c8
MecIntyre <abuesswalker@gmail.com>
2022-12-08 15:01:59 +0100

    Aufgabe Arrays Anfang

---
+++ 3_Erweiterte_Grundlagen/04_Aufgabe_Arrays.js
@@ -0,0 +1,56 @@
+"use strict"
+/*
+Hier werden die Teilnehmerlisten für 2 Sprachkurse definiert.
+Die \x-Schreibweise ist nur eine andere Schreibweise für normale Buchstaben - 
+sie dient dazu, dass du nicht schon vorab siehst, welcher Teilnehmer in welchem Kurs ist.
+
+\x58 stünde z.B.: für ein X.
+
+*Challenge* Versuche, die Aufgabe zu lösen, ohne dir die Variable "english1" oder "english2" auszugeben!
+
+Ein console.log(english2.length) oder so ist aber vollkommen ok.
+Versuche nur ein console.log(english1) oder console.log(english2) zu vermeiden! */
+
+let english1 = [
+    "\x4c\x75\x7A\x69\x61",
+    "\x48\x69\x61\x6E\x61",
+    "\x42\x65\x6E\x65\x64\x65\x74\x74\x6F",
+    "\x47\xFC\x6E\x74\x68\x65\x72"
+];
+
+let english2 = [
+    "\x57\x69\x62\x6B\x65",
+    "\x4A\x65\x61\x6E\x6E\x65\x74\x74\x65",
+    "\x50\x61\x75\x6C"
+]
+
+/* Aufgabe!
+
+Bei einer Sprachschule werden 2 English-Kurse unterrichtet.
+
+a)  Monika möchte unbedingt mit ihrem Freund "Paul" einen Sprachkurs belegen.
+    Finde dazu heraus, welchen Sprachkurs "Paul" belegt hat und füge sie ans Ende der Teilnehmerliste
+    und entferne ihn aus der Liste. */
+console.log("Lösung a:")
+
+/*
+b) Stelle sicher, dass "Günther" aus der Liste entfernt wurde - geht das, ohne die gesamte Liste per console.log auszugeben? */
+console.log("\nLösung b:")
+
+/*
+c)  Damit vor der Klasse die Anwesenheit kntrolliert werden kann, müssen die beiden Teilnehmerlisten sortiert werden
+    Stelle dies sicher! */
+console.log("\nLösung c:")
+
+/*
+d)  Ganz spontan möchte noch "Anton" am Sprachkurs teilnehmen.
+    Sein Name beginnt glücklicherweise mit "A"
+    Füge ihn daher an den Anfang der Liste vom Sprachkurs "english1" hinzu! */
+console.log("\nLösung d:")
+
+/*
+e)  Wie viele Teilnehmer sind jetzt in der Liste "english1" & "english2"?
+    Versuche erst jetzt, dir die Liste auszugeben und das Verhalten vom Programm 
+    mit console.log-Befehlen nachzuvollziehen. */
+console.log("\nLösung e:")
+
