e9d5c1a443b8a27c0c9e00a6503a90d30d741324
MecIntyre <abuesswalker@gmail.com>
2022-12-08 10:49:38 +0100

    Lösung 1 & 2

--- 2_Weitere_Grundlagen/06_Aufgabe_Math.js
+++ 2_Weitere_Grundlagen/06_Aufgabe_Math.js
@@ -7,7 +7,7 @@ Für dieses Beispiel soll ein Betrag von 15,37€ zurückgegeben werden.
 Wandle zuerst den Beitrag in die englische Schreibweise um (15.37).
 Ersetze dazu mit Hilfe von .replace() das Komma durch einen Punkt.
 
-Lese anschließend den String mit Hilfe der parseFloat()-Funktion ein und rechte ihn in Cent um.
+Lese anschließend den String mit Hilfe der parseFloat()-Funktion ein und rechne ihn in Cent um.
 
 Gebe dann aus, mit welchen Münzen dieser Betrag bezahlt werden kann.
 Der Einfachheit halber wird dieser Betrag ausschließlich mit Münzen bezahlt (vereinfacht den Code etwas).
@@ -31,10 +31,34 @@ Beispielausgabe:
  - 1x 2 Cent Münze
  - 0x 2 Cent Münze */
 
-console.log("Lösung 1: \n")
+console.log("Lösung 1:")
 let amountStr = "15,37"
 
+let amount = parseFloat(amountStr.replace(",", ".")) * 100
+console.log(amount)
 
+console.log(" - " + Math.floor(amount / 200) + "x 2€")
+amount = amount - Math.floor(amount / 200) * 200
+
+console.log(" - " + Math.floor(amount / 100) + "x 1€")
+amount = amount - Math.floor(amount / 100) * 100
+
+console.log(" - " + Math.floor(amount / 50) + "x 50 Cent")
+amount = amount - Math.floor(amount / 50) * 50
+
+console.log(" - " + Math.floor(amount / 20) + "x 20 Cent")
+amount = amount - Math.floor(amount / 20) * 20
+
+console.log(" - " + Math.floor(amount / 10) + "x 10 Cent")
+amount = amount - Math.floor(amount / 10) * 10
+
+console.log(" - " + Math.floor(amount / 5) + "x 5 Cent")
+amount = amount - Math.floor(amount / 5) * 5
+
+console.log(" - " + Math.floor(amount / 2) + "x 2 Cent")
+amount = amount - Math.floor(amount / 2) * 2
+
+console.log(" - " + amount + "x 1 Cent")
 
 /* Aufgabe 2:
  
@@ -53,4 +77,9 @@ let amountStr = "15,37"
 
  Das Programm muss nicht perfekt sein. Es reicht, wenn es ungefähr funktioniert. */
 
-console.log("Lösung 2: \n")
+console.log("\nLösung 2:")
+
+let bill = 17.00
+let billTotal = (Math.ceil(bill * 1.1 / 4) * 4)
+
+console.log("Der Gesamtbetrag beträgt $" + billTotal.toFixed(2) + ".")
