1b750bf9cd333d20c282593a4c0f77dc5b3d082d
MecIntyre <abuesswalker@gmail.com>
2022-12-08 11:13:05 +0100

    Weitere Variablenarten

---
+++ 2_Weitere_Grundlagen/07_Variablen3.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Nachwievor gültig aber veraltet und kann Bugs verursachen
+var hello = "Hallo Welt!"
+console.log(hello)
+
+// Allgemeingültig und sinnvoller als var
+let hallo = "Hallo Welt!"
+console.log(hallo)
+
+// Nichtveränderbare Variable --> behält den Urpsrungswert
+const greeting = "Hallo Welt!"
+console.log(greeting)
