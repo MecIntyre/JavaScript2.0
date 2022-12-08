d51b9494796ac027d2a1246781755d9efff4e06f
MecIntyre <abuesswalker@gmail.com>
2022-12-08 11:13:14 +0100

    Kommentarfunktionen

---
+++ 2_Weitere_Grundlagen/08_Kommentar.js
@@ -0,0 +1,8 @@
+//  Das ist ein einzeiliger Kommentar
+
+/*  Das ist ein
+    mehrzeiliger Kommentar */
+
+/** Das ist ein mehrzeiliges Kommentarfeld für den Editor z.B. VS-Code */
+const message = "Hallo Welt!"
+console.log(message)
