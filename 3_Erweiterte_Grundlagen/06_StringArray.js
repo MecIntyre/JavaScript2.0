453d0993d6fb7446b00a044a9684d5c670c789fb
MecIntyre <abuesswalker@gmail.com>
2022-12-12 10:00:22 +0100

    Array --> String --> Array

---
+++ 3_Erweiterte_Grundlagen/06_StringArray.js
@@ -0,0 +1,9 @@
+"Use strict"
+
+console.log("\nString zu Array: ")
+let shopping = "Apfel, Banane, Pfirsich"
+console.log(shopping.split(", "))
+
+console.log("\nArray zu String: ")
+let shoppingList = ['Apfel', 'Banane', 'Pfirsich']
+console.log(shoppingList.join(", "))
