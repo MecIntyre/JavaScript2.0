2a24aaf10b1b7c5778ae8f2daac65e85694cd0c2
MecIntyre <abuesswalker@gmail.com>
2022-12-12 12:51:52 +0100

    Einführung Vergleiche

---
+++ 4_Kontrollstrukturen/01_ExkursBoleans.js
@@ -0,0 +1,22 @@
+"Use strict"
+let greeting = "Hallo Welt"
+
+// Exkurs Boleans: Variablen, die true oder false enthalten
+console.log("\nVergleiche:")
+let a = (greeting + "!") === "Hallo Welt!"
+let b = (greeting + "!") !== "Hallo Welt!"
+
+console.log("a: ", a)
+console.log("b: ", b)
+
+// Mathematische Vergleiche
+console.log("\nMathematische Vergleiche:")
+let zahl1 = 5
+let zahl2 = 6
+
+console.log("zahl1 === zahl2: ", zahl1 === zahl2)
+console.log("zahl1 !== zahl2: ", zahl1 !== zahl2)
+console.log("zahl1 < zahl2: ", zahl1 < zahl2)
+console.log("zahl1 <= zahl2: ", zahl1 <= zahl2)
+console.log("zahl1 > zahl2: ", zahl1 > zahl2)
+console.log("zahl1 >= zahl2: ", zahl1 >= zahl2)
