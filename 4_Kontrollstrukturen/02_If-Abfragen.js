e58941f64221c383acccd0ea1327ed6a58ee8731
MecIntyre <abuesswalker@gmail.com>
2022-12-12 13:33:55 +0100

    If bedingungen

---
+++ 4_Kontrollstrukturen/02_If-Abfragen.js
@@ -0,0 +1,20 @@
+"use strict"
+/*
+let a = true
+
+if (a) {
+    console.log("Hallo Welt!")
+} */
+
+/*
+let zahl1 = 7
+let zahl2 = 6
+
+if (zahl1 <= zahl2) {
+    console.log("zahl1 ist kleiner gleich zahl2!")
+} */
+
+let students = ["Max", "Moritz"]
+if (students.indexOf("Erika") === -1) {
+    console.log("Erika nimmt noch nicht am Kurs teil!")
+}
