b39ccf5e33db035030e2a6cf3864030c390714b8
MecIntyre <abuesswalker@gmail.com>
2022-12-12 15:08:00 +0100

    Coeblocks & Bündel

---
+++ 4_Kontrollstrukturen/04_CodeBlock.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/*
+{
+    console.log("Zeile 1")
+    console.log("Zeile 2")
+} */
+
+
+
+let a = 41
+
+// Gültige Schreibweise
+if (a === 42) console.log("Zeie 0!")
+
+// Gültige & sinnvolle Schreibweise
+if (a === 42) {
+    console.log("Zeile 1")
+    console.log("Zeile 2")
+}
