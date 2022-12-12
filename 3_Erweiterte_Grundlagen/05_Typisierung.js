cb2658bd1ff16dae7b6e4fdfe489e24d2c197bb2
MecIntyre <abuesswalker@gmail.com>
2022-12-12 09:53:33 +0100

    Verschachtelung von Arrays

---
+++ 3_Erweiterte_Grundlagen/05_Typisierung.js
@@ -0,0 +1,11 @@
+"use Strict"
+
+// Arrays sind nicht typisiert und Verschachtelungen grundsätzlich erlaubt aber nicht sinnvoll
+
+let liste = [
+    "Banane",
+    4,
+    ["Element 1", "Element 2"]
+]
+
+console.log(liste[2][0])
