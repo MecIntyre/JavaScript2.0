fdfc09da0f6f4a3488a9d705996210f6135b904b
MecIntyre <abuesswalker@gmail.com>
2022-12-14 10:55:48 +0100

    Variablen in If-Bedingungen

---
+++ 4_Kontrollstrukturen/08_If_Variable.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let request = null
+request = "GET /"
+
+/*
+JavaScript erkennt automatisch, dass die Variable ein String ist und einen Wert enthält.
+Daraufhin wird die Variable in der If-Abfrage direkt jenach Wert, in ein True/False umgewandelt. */
+
+if (request) {
+    console.log("Hallo Welt!")
+}
