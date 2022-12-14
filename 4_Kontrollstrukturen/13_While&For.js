447f6752fc88385bb29eb0e4d9b50fd4f904d9dc
MecIntyre <abuesswalker@gmail.com>
2022-12-14 16:44:17 +0100

    Vergleich FOr & While Schleife

---
+++ 4_Kontrollstrukturen/13_While&For.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// While-Schleife
+console.log("\nWhile-Schleife:")
+let counter1 = 0
+
+while (counter1 < 5) {
+    counter1++
+    console.log("Counter1: ", counter1)
+}
+
+// For-Schleife
+console.log("\nFor-Schleife:")
+for (let counter2 = 0; counter2 < 11; counter2++) {
+    console.log("Counter2: ", counter2)
+}
