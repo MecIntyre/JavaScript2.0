735687832e6e865160fd39f18e4fd06de0a8d026
MecIntyre <abuesswalker@gmail.com>
2022-12-14 10:39:37 +0100

    Operatoren Prioritäten

--- 4_Kontrollstrukturen/07_Reihenfolge.js
+++ 4_Kontrollstrukturen/07_Reihenfolge.js
@@ -1,3 +1,15 @@
 "use strict"
 
-console.log(5 + 4 === 10)
+/* 
+Hier gilt ebenfals Punkt vor Strich und danach erst Vergleiche 
+Klammern helfen ebenfalls, die Prioritäten klar zu definieren. */
+
+console.log(5 + 3 * 5)
+console.log(5 + 5 === 10)
+
+if (
+    ((5 + 5) === 10) &&
+    ((3 * 5) === 15)
+) {
+
+}
